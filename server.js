c2f1702b80287bc296c7b226357e45a57857a873
JakeSchlei <jakeschlei@gmail.com>
2022-09-26 13:53:04 -0700

    able to now take notes and save to the db file and delete notes

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 // require express
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
-
+const path = require('path');
+const uniqid = require('uniqid')
+const notes = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 
 
@@ -14,14 +18,46 @@ app.get('/notes', (req,res) => {
 });
 
 
+
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, "./db/db.json"), 'utf8', (err, data) => {
+        res.json(JSON.parse(data));
+    });
+});
+
+app.post('/api/notes', (req, res) => {
+    let newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uniqid()
+    };
+  
+
+    notes.push(newNote);
+    
+    fs.writeFile(`${__dirname}/db/db.json`, JSON.stringify(notes, null, 2), (err) => {
+        if (err) {
+            return res.status(500).json({err});
+        }
+        res.json(notes);
+    });
+
+});
+
+app.delete('/api/notes/:id', (req,res) => {
+    let db = JSON.parse(fs.readFileSync('./db/db.json'))
+    let deleteNote = db.filter(item => item.id !== req.params.id);
+    fs.writeFileSync('./db/db.json', JSON.stringify(deleteNote));
+    res.json(deleteNote);
+})
+
+
+
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-
-
-
-
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
